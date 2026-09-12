65f3105b2f6e4b7dd350b592c822d79c9de6eeec
npark2860-cyber <npark2860@gmail.com>
2026-09-12 18:49:47 +0900

    diag: move probe after frame dummy

--- source/pre_main_probe.c
+++ source/pre_main_probe.c
@@ -2,7 +2,7 @@
 
 static volatile u32 g_pre_main_probe_state;
 
-__attribute__((constructor(101))) static void nce_diag_pre_main_constructor_probe(void) {
+__attribute__((constructor)) static void nce_diag_pre_main_constructor_probe(void) {
     static const char enter[] = "[NCE-DIAG][BOOT] 40_CTOR_ENTER\n";
     static const char done[] = "[NCE-DIAG][BOOT] 41_CTOR_DONE\n";
     static const char witness[] = "[NCE-DIAG][BOOT] 42_POST_SECOND_RETURN\n";
